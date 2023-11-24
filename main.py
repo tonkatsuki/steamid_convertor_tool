45646c95659c28207aba7aa67b1d4ec55df02808
jazu <6411441+tonkatsuki@users.noreply.github.com>
2023-11-24 16:10:01 -0600

    add def for math to convert sid

--- main.py
+++ main.py
@@ -8,6 +8,20 @@
 # python3 main.py admins.cfg
 # it will export admins_new.cfg
 #
-#
-#
 
+# Global vars
+steamid64ident = 76561197960265728
+
+
+def steamid_to_commid(steamid):
+    sid_split = steamid.split(":")
+    commid = int(sid_split[2]) * 2
+
+    if sid_split[1] == "1":
+        commid += 1
+
+    commid += steamid64ident
+    return commid
+
+
+print(steamid_to_commid("STEAM_0:1:1934414"))
